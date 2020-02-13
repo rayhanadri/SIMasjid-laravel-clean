b6024d0d96f0d019ecbbfbdb81b5c6f8dfcc343d
rayhanadri <rayerzz99@gmail.com>
2020-02-13 07:58:11 +0700

    add authenticatesusers.php, update readme

--- simasjid-clean-min.sql
+++ simasjid-clean-min.sql
@@ -1,83 +1,149 @@
--- MySQL Workbench Forward Engineering
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 13, 2020 at 01:41 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.1.31
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='';
-
--- -----------------------------------------------------
--- Schema simasjid
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema simasjid
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `simasjid` DEFAULT CHARACTER SET utf8 ;
-USE `simasjid` ;
-
--- -----------------------------------------------------
--- Table `simasjid`.`anggota`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `simasjid`.`anggota` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `id_jabatan` INT NULL,
-  `id_status` INT NULL,
-  `username` VARCHAR(20) NULL,
-  `password` VARCHAR(255) NULL,
-  `nama` VARCHAR(255) NULL,
-  `alamat` TEXT NULL,
-  `telp` VARCHAR(20) NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `link_foto` VARCHAR(255) NULL DEFAULT 'public/dist/assets/img/avatar/avatar-1.png',
-  `remember_token` VARCHAR(100) NULL,
-  `created_at` TIMESTAMP NULL,
-  `updated_at` TIMESTAMP NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC))
-ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `simasjid`.`password_resets`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `simasjid`.`password_resets` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(255) NULL,
-  `token` VARCHAR(255) NULL,
-  `created_at` TIMESTAMP NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `simasjid`
+--
 
--- -----------------------------------------------------
--- Table `simasjid`.`notifikasi`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `simasjid`.`notifikasi` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `id_pembuat` INT NULL,
-  `id_penerima` INT NOT NULL,
-  `jenis` VARCHAR(100) NULL,
-  `msg` VARCHAR(255) NULL,
-  `sudah_baca` INT NULL DEFAULT 0,
-  `icon` VARCHAR(100) NULL,
-  `bg` VARCHAR(100) NULL,
-  `link` VARCHAR(255) NULL,
-  `tgl_dibuat` TIMESTAMP NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_notifikasi_anggota1_idx` (`id_pembuat` ASC),
-  INDEX `fk_notifikasi_anggota2_idx` (`id_penerima` ASC),
-  CONSTRAINT `fk_notifikasi_anggota1`
-    FOREIGN KEY (`id_pembuat`)
-    REFERENCES `simasjid`.`anggota` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_notifikasi_anggota2`
-    FOREIGN KEY (`id_penerima`)
-    REFERENCES `simasjid`.`anggota` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `anggota`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+CREATE TABLE `anggota` (
+  `id` int(11) NOT NULL,
+  `id_jabatan` int(11) DEFAULT NULL,
+  `id_status` int(11) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `telp` varchar(20) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `link_foto` varchar(255) DEFAULT 'public/dist/assets/img/avatar/avatar-1.png',
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `anggota`
+--
+
+INSERT INTO `anggota` (`id`, `id_jabatan`, `id_status`, `username`, `password`, `nama`, `alamat`, `telp`, `email`, `link_foto`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 'ketua', '$2y$10$oX2b5kO9NK2Bjvia2dPBr.LKwn2QKYeDNCSBAmb2NbNggwASM6Vvm', 'Pak Ketua', 'Malang', '08123123456', 'ketua@gmail.com', 'public/storage/foto_profil/1.png', NULL, '2020-02-12 23:40:42', '2020-02-13 00:27:58'),
+(2, 4, 1, 'takmir1', '$2y$10$clXnYwpCa/i8oJL99Vao5eVC6wdebgaiVwk6n4HNInK4xUfh6ZHN2', 'Takmir Satu', 'Malang', '08123123456', 'takmir1@gmail.com', 'public/dist/assets/img/avatar/avatar-1.png', NULL, '2020-02-12 23:41:46', '2020-02-12 23:41:46'),
+(3, 4, 1, 'remas1', '$2y$10$opBN/04FBm7RIpyf2.2PZ.JroQBSjxHRdbSWvPSCnzdVx8jKzDZqu', 'Remas Satu', 'Malang', '08123123456', 'remas123@gmail.com', 'public/dist/assets/img/avatar/avatar-1.png', NULL, '2020-02-12 23:42:33', '2020-02-12 23:42:33');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifikasi`
+--
+
+CREATE TABLE `notifikasi` (
+  `id` int(11) NOT NULL,
+  `id_pembuat` int(11) DEFAULT NULL,
+  `id_penerima` int(11) NOT NULL,
+  `jenis` varchar(100) DEFAULT NULL,
+  `msg` varchar(255) DEFAULT NULL,
+  `sudah_baca` int(11) DEFAULT 0,
+  `icon` varchar(100) DEFAULT NULL,
+  `bg` varchar(100) DEFAULT NULL,
+  `link` varchar(255) DEFAULT NULL,
+  `tgl_dibuat` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `anggota`
+--
+ALTER TABLE `anggota`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email_UNIQUE` (`email`);
+
+--
+-- Indexes for table `notifikasi`
+--
+ALTER TABLE `notifikasi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_notifikasi_anggota1_idx` (`id_pembuat`),
+  ADD KEY `fk_notifikasi_anggota2_idx` (`id_penerima`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `anggota`
+--
+ALTER TABLE `anggota`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `notifikasi`
+--
+ALTER TABLE `notifikasi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `notifikasi`
+--
+ALTER TABLE `notifikasi`
+  ADD CONSTRAINT `fk_notifikasi_anggota1` FOREIGN KEY (`id_pembuat`) REFERENCES `anggota` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_notifikasi_anggota2` FOREIGN KEY (`id_penerima`) REFERENCES `anggota` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
