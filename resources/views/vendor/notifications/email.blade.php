7612200e27228b31986d573232badb91f6e45ccd
rayhanadri <rayerzz99@gmail.com>
2020-02-15 17:46:09 +0700

    security fix, ganti email reset password

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -4,9 +4,9 @@
 # {{ $greeting }}
 @else
 @if ($level === 'error')
-# @lang('Whoops!')
+# @lang('Oops!')
 @else
-# @lang('Hello!')
+# @lang('Ahlan wa sahlan!')
 @endif
 @endif
 
@@ -43,7 +43,7 @@
 @if (! empty($salutation))
 {{ $salutation }}
 @else
-@lang('Regards'),<br>
+@lang('Salam'),<br>
 {{ config('app.name') }}
 @endif
 
@@ -51,8 +51,8 @@
 @isset($actionText)
 @slot('subcopy')
 @lang(
-    "If you’re having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-    'into your web browser: [:actionURL](:actionURL)',
+    "Jika Anda kesulitan dalam mengklik \":actionText\" tombol tersebut, copy dan paste link di bawah ini\n".
+    'ke halaman browser Anda: [:actionURL](:actionURL)',
     [
         'actionText' => $actionText,
         'actionURL' => $actionUrl,
